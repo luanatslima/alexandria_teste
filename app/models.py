debc4955788161b9ec63e196cdcd3bf2eedb446f
Luana <luanatayna2007@gmail.com>
2025-01-07 17:20:00 -0300

    forms para login

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model): 
+    email = models.EmailField() 
+    senha = models.CharField(max_length=11) 
+    
+    def __str__(self): 
+        return self.email
--- app/views.py
+++ app/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UsuarioForm
 
 def login(request):
-    return render(request, "login.html")
+    if request.method == 'POST':
+        form = UsuarioForm(request.POST)
+        if form.is_valid():
+            # Processar os dados do formulário
+            email = form.cleaned_data['email']
+            senha = form.cleaned_data['senha']
+            # Aqui você pode adicionar a lógica para autenticar o usuário
+            # Por exemplo, verificar se o email e a senha estão corretos
+            return redirect('logado')  # Redirecionar para uma página de sucesso
+    else:
+        form = UsuarioForm()
+    return render(request, 'login.html', {'form': form})
+
+def logado(request): 
+    return render(request, 'logado.html')
